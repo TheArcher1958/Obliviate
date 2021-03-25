bacf40a7b42884fdb824020987ccb26e93151d68
The_Archer <harrypotterhp333@gmail.com>
2021-03-24 23:10:53 -0600

    First commit. Added Firebase to the project, am getting data from Firestore.

---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+
+import 'dart:async';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_analytics/observer.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Firebase Analytics Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  //MyHomePage({Key key}) : super(key: key);
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    CollectionReference users = FirebaseFirestore.instance.collection('users');
+
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        child: StreamBuilder<QuerySnapshot>(
+          stream: users.snapshots(),
+          builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (snapshot.hasError) {
+              return Text('Something went wrong');
+            }
+
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Text("Loading");
+            }
+
+            return new ListView(
+              children: snapshot.data.docs.map((DocumentSnapshot document) {
+                return Column(children: [
+                  Text(document.data()['name']),
+                  Text(document.data()['age']),
+                ],);
+              }).toList(),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
